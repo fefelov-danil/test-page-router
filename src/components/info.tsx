858ecab8059b57bc2b7cc7f3b396709a8e9fc0d2
Daniil Fefelov <fefelov.danil@gmail.com>
2023-12-02 13:30:27 +0400

    Page route

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -102,7 +102,7 @@ export const Info = ({characters}: Props) => {
 
   return (
     <div className="p-10 bg-gray-600">
-      <h1 className="text-2xl text-center mb-5">App router. Без клиентских компонент.</h1>
+      <h1 className="text-2xl text-center mb-5">Page router.</h1>
       <h2 className="text-center my-5">Количество пресонажей: {characters.info.count}</h2>
       <div className="grid grid-cols-3 gap-4 max-w-[1000px] mx-auto">
         {characters.results.map((character, i) => <Character key={i} character={character} />)}
