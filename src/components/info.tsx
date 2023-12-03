3e0a396cef4d4318521492df9fe251f42da5a624
Daniil Fefelov <fefelov.danil@gmail.com>
2023-12-03 17:25:05 +0400

    Page route

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -1,9 +1,102 @@
 import {Character} from "@/components/character";
+import Image from 'next/image'
+import Badge from "@mui/material/Badge";
+import MailIcon from '@mui/icons-material/Mail';
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import CircularProgress from '@mui/material/CircularProgress';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ListItemText from "@mui/material/ListItemText";
 import {ICharacter} from "@/common/api";
 
 interface Props {
   characters: ICharacter
 }
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    title: 'Basketball',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike',
+  },
+];
 
 export const Info = ({characters}: Props) => {
 
@@ -13,6 +106,202 @@ export const Info = ({characters}: Props) => {
       <h2 className="text-center my-5">Количество пресонажей: {characters.info.count}</h2>
       <div className="grid grid-cols-3 gap-4 max-w-[1000px] mx-auto">
         {characters.results.map((character, i) => <Character key={i} character={character} />)}
+      </div>
+      <div className="my-10">
+        <Badge badgeContent={4} color="primary">
+          <MailIcon color="action" />
+        </Badge>
+      </div>
+      <div className="my-10">
+        <List component="nav" aria-label="mailbox folders">
+          <ListItem button>
+            <ListItemText primary="Inbox" />
+          </ListItem>
+          <Divider />
+          <ListItem button divider>
+            <ListItemText primary="Drafts" />
+          </ListItem>
+          <ListItem button>
+            <ListItemText primary="Trash" />
+          </ListItem>
+          <Divider light />
+          <ListItem button>
+            <ListItemText primary="Spam" />
+          </ListItem>
+        </List>
+      </div>
+      <div className="my-10">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.calories}</TableCell>
+                  <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      <div className="my-10">
+        <Box sx={{ width: 500 }}>
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Tooltip title="Add" placement="top-start">
+                <Button>top-start</Button>
+              </Tooltip>
+              <Tooltip title="Add" placement="top">
+                <Button>top</Button>
+              </Tooltip>
+              <Tooltip title="Add" placement="top-end">
+                <Button>top-end</Button>
+              </Tooltip>
+            </Grid>
+          </Grid>
+          <Grid container justifyContent="center">
+            <Grid item xs={6}>
+              <Tooltip title="Add" placement="left-start">
+                <Button>left-start</Button>
+              </Tooltip>
+              <br />
+              <Tooltip title="Add" placement="left">
+                <Button>left</Button>
+              </Tooltip>
+              <br />
+              <Tooltip title="Add" placement="left-end">
+                <Button>left-end</Button>
+              </Tooltip>
+            </Grid>
+            <Grid item container xs={6} alignItems="flex-end" direction="column">
+              <Grid item>
+                <Tooltip title="Add" placement="right-start">
+                  <Button>right-start</Button>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="Add" placement="right">
+                  <Button>right</Button>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Tooltip title="Add" placement="right-end">
+                  <Button>right-end</Button>
+                </Tooltip>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Tooltip title="Add" placement="bottom-start">
+                <Button>bottom-start</Button>
+              </Tooltip>
+              <Tooltip title="Add" placement="bottom">
+                <Button>bottom</Button>
+              </Tooltip>
+              <Tooltip title="Add" placement="bottom-end">
+                <Button>bottom-end</Button>
+              </Tooltip>
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
+      <div className="my-10">
+        <Box sx={{ width: '100%', maxWidth: 500 }}>
+          <Typography variant="h1" gutterBottom>
+            h1. Heading
+          </Typography>
+          <Typography variant="h2" gutterBottom>
+            h2. Heading
+          </Typography>
+          <Typography variant="h3" gutterBottom>
+            h3. Heading
+          </Typography>
+          <Typography variant="h4" gutterBottom>
+            h4. Heading
+          </Typography>
+          <Typography variant="h5" gutterBottom>
+            h5. Heading
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            h6. Heading
+          </Typography>
+          <Typography variant="subtitle1" gutterBottom>
+            subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+            blanditiis tenetur
+          </Typography>
+          <Typography variant="subtitle2" gutterBottom>
+            subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+            blanditiis tenetur
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+            blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+            neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+            quasi quidem quibusdam.
+          </Typography>
+          <Typography variant="body2" gutterBottom>
+            body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+            blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
+            neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
+            quasi quidem quibusdam.
+          </Typography>
+          <Typography variant="button" display="block" gutterBottom>
+            button text
+          </Typography>
+          <Typography variant="caption" display="block" gutterBottom>
+            caption text
+          </Typography>
+          <Typography variant="overline" display="block" gutterBottom>
+            overline text
+          </Typography>
+        </Box>
+      </div>
+      <div className="my-10">
+        <Stack sx={{ width: '100%' }} spacing={2}>
+          <Alert severity="error">This is an error alert — check it out!</Alert>
+          <Alert severity="warning">This is a warning alert — check it out!</Alert>
+          <Alert severity="info">This is an info alert — check it out!</Alert>
+          <Alert severity="success">This is a success alert — check it out!</Alert>
+        </Stack>
+      </div>
+      <div className="my-10">
+        <Stack sx={{ color: 'grey.500' }} spacing={2} direction="row">
+          <CircularProgress color="secondary" />
+          <CircularProgress color="success" />
+          <CircularProgress color="inherit" />
+        </Stack>
+      </div>
+      <div className="my-10">
+        <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <Image
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                alt={item.title}
+                width={164}
+                height={164}
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
       </div>
     </div>
   )
